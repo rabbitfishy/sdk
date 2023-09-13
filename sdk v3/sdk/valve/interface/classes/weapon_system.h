cc3be5f4f03317b937a9d7fe05e8eb3f649c9d35
rabbitfishy <144937211+rabbitfishy@users.noreply.github.com>
2023-09-13 17:23:35 +0100

    Delete sdk v3 directory

--- sdk v3/sdk/valve/interface/classes/weapon_system.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class game_weapon_system
-{
-public:
-	game_weapon_info*	weapon_data(short item_definition_index) { return virtuals->call<game_weapon_info*>(this, 2, item_definition_index); };
-};
