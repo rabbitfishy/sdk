a2e0ab57fde019db96fabd67a9a2ee6710a3043e
rabbitfishy <144937211+rabbitfishy@users.noreply.github.com>
2025-06-02 10:19:20 +0100

    Update maths.h

--- sdk/dependencies/maths/maths.h
+++ sdk/dependencies/maths/maths.h
@@ -10,6 +10,7 @@ public:
 	bool world_to_screen(const vector_3d& origin, vector_2d& screen);
 	vector_3d vector_transform(vector_3d& transform, matrix_3x4& matrix);
 	void transform_vector(vector_3d& start, matrix_3x4& matrix, vector_3d& end);
+	matrix_3x4 angle_matrix(const q_angle& angle);
 
 	void normalize_angle(float& angle);
 	void angle_vectors(const q_angle& angles, vector_3d* forward, vector_3d* right = nullptr, vector_3d* up = nullptr);
